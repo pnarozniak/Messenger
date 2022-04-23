397e82ad3afe69f714acc4a2016c25f0071c9a52
xlegodroit <xxvege@gmail.com>
2022-04-23 02:25:00 +0200

    Added core db migration

--- MessengerApi/Database/Migrations/20220423002435_added_core_tables.Designer.cs
+++ MessengerApi/Database/Migrations/20220423002435_added_core_tables.Designer.cs
@@ -4,6 +4,7 @@ using MessengerApi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MessengerApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220423002435_added_core_tables")]
+    partial class added_core_tables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
